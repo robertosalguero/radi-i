d90ee1da4a22ef7e5ee0009793de34d15925700b
rob <robertsalguerojr@gmail.com>
2018-09-13 19:35:32 -0400

    adjust cors

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,13 @@ Bundler.require(*Rails.groups)
 module RadiI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+  config.load_defaults 5.2
+  config.middleware.insert_before 0, Rack::Cors do
+	  allow do
+		  origins '*'
+		  resource '*', headers: :any, methods: [:get, :post, :options]
+	  end
+  end
 
 
     # Settings in config/environments/* take precedence over those specified here.
