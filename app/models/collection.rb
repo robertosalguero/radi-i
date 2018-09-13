cb79417f95684847e8414de9d3570a88801bee01
rob <robertsalguerojr@gmail.com>
2018-09-13 19:24:26 -0400

    fix model typo

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,4 @@
 class Collection < ApplicationRecord
-    has_many :content, dependent :destroy
-    has_many :placements, dependent :destroy
+    has_many :content, dependent: :destroy
+    has_many :placements, dependent: :destroy
 end
